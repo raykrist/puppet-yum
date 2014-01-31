d88159f9fbf48efde903f349b4bc08aa50a14b44
Raymond Kristiansen <raymond@nexusweb.no>
2014-01-31 20:47:52 +0100

    Added puppetlabs repo

---
+++ manifests/repo/puppetlabs.pp
@@ -0,0 +1,26 @@
+class yum::repo::puppetlabs(
+  $el_version = 6
+) {
+
+  yum::repo { 'puppetlabs-products':
+    desc            => "Puppet Labs Products El ${el_version} - \$basearch",
+    baseurl         => "http://yum.puppetlabs.com/el/${el_version}/products/\$basearch",
+    enabled         => 1,
+    gpgcheck        => 1,
+    gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs'
+  }
+
+  yum::repo { 'puppetlabs-deps':
+    desc            => "Puppet Labs Dependencies El ${el_version} - \$basearch",
+    baseurl         => "http://yum.puppetlabs.com/el/${el_version}/dependencies/\$basearch",
+    enabled         => 1,
+    gpgcheck        => 1,
+    gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs'
+  }
+
+  # Add gpgkey file
+  yum::gpgkey { 'RPM-GPG-KEY-puppetlabs':
+    source => 'puppet:///modules/yum/RPM-GPG-KEY-puppetlabs',
+  }
+
+}
