b2b14dbc5851b107380b311b44b906894440c25e
Raymond Kristiansen <raymond@nexusweb.no>
2014-10-19 14:05:29 +0200

    EPEL repo is now based on releasever yum variable

--- manifests/repo/epel.pp
+++ manifests/repo/epel.pp
@@ -1,26 +1,28 @@
 class yum::repo::epel() {
 
+  $releasever = $::operatingsystemmajrelease
+
   yum::repo { 'epel':
-    desc            => 'Extra Packages for Enterprise Linux 6 - $basearch',
-    mirrorlist      => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+    desc            => "Extra Packages for Enterprise Linux ${$releasever} - \$basearch",
+    mirrorlist      => "https://mirrors.fedoraproject.org/metalink?repo=epel-${$releasever}&arch=\$basearch",
     enabled         => 1,
     gpgcheck        => 1,
     autokeyimport   => 'yes',
     gpgkey_name     => 'epel@fedoraproject.org',
-    gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6'
+    gpgkey          => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${$releasever}"
   }
 
   yum::repo { 'epel-testing':
-    desc            => 'Extra Packages for Enterprise Linux 6 - Testing - $basearch',
-    mirrorlist      => 'https://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch',
+    desc            => "Extra Packages for Enterprise Linux  ${$releasever}- Testing - \$basearch",
+    mirrorlist      => "https://mirrors.fedoraproject.org/metalink?repo=testing-epel${$releasever}&arch=\$basearch",
     enabled         => 0,
     gpgcheck        => 1,
-    gpgkey          => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6'
+    gpgkey          => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${$releasever}"
   }
 
   # Add gpgkey file
-  yum::gpgkey { 'RPM-GPG-KEY-EPEL-6':
-    source => 'puppet:///modules/yum/RPM-GPG-KEY-EPEL-6',
+  yum::gpgkey { "RPM-GPG-KEY-EPEL-${$releasever}":
+    source => "puppet:///modules/yum/RPM-GPG-KEY-EPEL-${$releasever}",
     before => Yum::Repo['epel']
   }
 
